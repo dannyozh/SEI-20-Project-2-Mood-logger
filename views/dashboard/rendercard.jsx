b41cf23e6db591b30a7da1462c5a8b410a20e00b
dannyozh <danielongzhihao@gmail.com>
2019-10-23 21:34:59 +0800

    added html text for profile card

--- views/dashboard/rendercard.jsx
+++ views/dashboard/rendercard.jsx
@@ -11,19 +11,67 @@ class Rendercard extends React.Component {
 						integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 						crossorigin="anonymous"
 					/>
+					<link href="//fonts.googleapis.com/css?family=Lato:400,300,100,700,900" rel="stylesheet" type="text/css" />
+					<link href="cardstyle.css" />
 				</head>
 				<body class="bg-primary">
-					<div class="container text-center text-white">
-						<h1>Moodlogger</h1>
+					<div class="container">
+						<h1 class="text-white text-center">Moodlogger</h1>
 						<hr />
-						<h3>Your profile</h3>
-
-						<form action="/logsaved" method="POST">
-							<div class="form-group">
-								<textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="log" />
+						<h3 class="text-white text-center"> Dear {this.props.username}, this is a summary of today's log.</h3>
+						<div class="card-group vgr-cards">
+							<div class="card">
+								<div class="card-img-body">
+									<img
+										class="card-img"
+										src="https://images.unsplash.com/photo-1500322969630-a26ab6eb64cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjExMjU4fQ&w=1000&q=80"
+										alt="Card image cap"
+									/>
+								</div>
+								<div class="card-body">
+									<h4 class="card-title">Mood Score: {this.props.mood}</h4>
+									<p class="card-text">
+										If someone decides they're not going to be happy, it's not your problem. You don't have to spend your time and energy
+										trying to cheer up someone who has already decided to stay in a bad mood. Believe it or not, you can actually hurt
+										people by playing into their self-pity. <b>Joyce Meyer</b>
+									</p>
+									<a href="#" class="btn btn-outline-primary">
+										Primary
+									</a>
+								</div>
 							</div>
-							<input type="submit" />
-						</form>
+							<div class="card">
+								<div class="card-img-body">
+									<img class="card-img" src="https://i.ytimg.com/vi/BgIJ45HKDpw/maxresdefault.jpg" alt="Card image cap" />
+								</div>
+								<div class="card-body">
+									<h4 class="card-title">How are you feeling: {this.props.feeling}</h4>
+									<p class="card-text">
+										“Every time you are tempted to react in the same old way, ask if you want to be a prisoner of the past or a pioneer of
+										the future.” – <b>Deepak Chopra</b>
+									</p>
+									<a href="#" class="btn btn-outline-primary">
+										Primary
+									</a>
+								</div>
+							</div>
+							<div class="card">
+								<div class="card-img-body">
+									<img
+										class="card-img"
+										src="https://images.unsplash.com/photo-1541890289-b86df5bafd81?ixlib=rb-1.2.1&w=1000&q=80"
+										alt="Card image cap"
+									/>
+								</div>
+								<div class="card-body">
+									<h4 class="card-title">Today's log:</h4>
+									<p class="card-text">{this.props.log}</p>
+									<a href="#" class="btn btn-outline-primary">
+										Primary
+									</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</body>
 			</html>
