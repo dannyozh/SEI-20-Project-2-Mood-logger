b3bedf84b8419cbfbdb67dcbcd5019ca6fdbbb8a
dannyozh <danielongzhihao@gmail.com>
2019-10-28 00:29:20 +0800

    remoived opacity from graph.

--- public/profilechart.js
+++ public/profilechart.js
@@ -13,22 +13,15 @@ var myChart = new Chart(ctx, {
 				label: 'Your mood level over the past 5 days:',
 				data: allmoods,
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgb(255, 99, 132)',
+					'rgb(54, 162, 235)',
+					'rgb(255, 206, 86)',
+					'rgb(75, 192, 192)',
+					'rgb(153, 102, 255)',
+					'rgb(255, 159, 64)'
 				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
+				borderColor: [ 'rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 206, 86)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)', 'rgb(255, 159, 64)' ],
+				borderWidth: 2
 			}
 		]
 	},
