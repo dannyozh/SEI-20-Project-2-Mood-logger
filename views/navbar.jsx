1edcb3a9098a1d1f7828503dcec6a43901d22ede
dannyozh <danielongzhihao@gmail.com>
2019-10-29 15:16:37 +0800

    added qotd functionality

--- views/navbar.jsx
+++ views/navbar.jsx
@@ -46,6 +46,11 @@ class Navbar extends React.Component {
 										Today's Log
 									</a>
 								</li>
+								<li class="nav-item">
+									<a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+										Quote Of The Day
+									</a>
+								</li>
 							</ul>
 							<ul class="navbar-nav ml-auto nav-flex-icons">
 								<li class="nav-item avatar">
@@ -61,6 +66,30 @@ class Navbar extends React.Component {
 							</ul>
 						</div>
 					</nav>
+					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-body">
+									<script type="text/javascript" src="https://www.brainyquote.com/link/quotebr.js" />
+								</div>
+							</div>
+						</div>
+					</div>
+					<script
+						src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+						integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+						crossorigin="anonymous"
+					/>
+					<script
+						src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+						integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+						crossorigin="anonymous"
+					/>
+					<script
+						src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+						integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+						crossorigin="anonymous"
+					/>
 				</body>
 			</html>
 		);
